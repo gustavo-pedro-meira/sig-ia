5a2d328d4f06e412907a80698c899b2a1ecbd0d7
gustavo-pedro-meira <gustavomeira@ads.fiponline.edu.br>
2025-09-22 11:59:01 -0300

    Validação se employee existe em DeleteEmployeeUseCase

--- src/modules/employee/useCases/delete-employee.usecase.ts
+++ src/modules/employee/useCases/delete-employee.usecase.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { EmployeeRepository } from "../repositories/employee.repository";
 
 @Injectable()
@@ -6,6 +6,12 @@ export class DeleteEmployeeUseCase {
     constructor(private readonly employeeRepository: EmployeeRepository) {}
 
     async execute(id: string) {
+        const employeeExist = await this.employeeRepository.findOne(id);
+
+        if(!employeeExist) {
+            throw new NotFoundException('Employee not found')
+        }
+        
         return await this.employeeRepository.deleteById(id);
     }
 }
