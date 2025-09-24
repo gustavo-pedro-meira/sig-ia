cb3af3e67c1b13583103df7837cdcd8e1cbb764f
gustavo-pedro-meira <gustavomeira@ads.fiponline.edu.br>
2025-09-24 00:22:05 -0300

    Tratando erros

--- src/modules/employee/useCases/update-employee.usecas.ts
+++ src/modules/employee/useCases/update-employee.usecas.ts
@@ -1,12 +1,22 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { EmployeeRepository } from "../repositories/employee.repository";
 import { UpdateEmployeeDto } from "../dto/update-employee.dto";
+import { hash } from "bcrypt";
 
 @Injectable()
 export class UpdateEmployeeUseCase {
     constructor(private readonly employeeRepository: EmployeeRepository) {}
 
     async execute(id: string, updateEmployeeDto: UpdateEmployeeDto) {
-        return await this.employeeRepository.updateById(id, updateEmployeeDto);
-    }
+        const employeeExist = await this.employeeRepository.findOne(id);
+
+        if(!employeeExist) {
+            throw new NotFoundException('Employee not found');
+        }
+
+        if(updateEmployeeDto.password) {
+            updateEmployeeDto.password = await hash(updateEmployeeDto.password, 10);
+        }
+
+        return await this.employeeRepository.updateById(id, updateEmployeeDto);    }
 }
